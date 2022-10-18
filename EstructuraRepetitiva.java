1048e6acf5a536d10a6551c0d1933654013b3238
Andriy00000 <116098056+Andriy00000@users.noreply.github.com>
2022-10-18 18:12:44 +0200

    Actualitzat

--- EstructuraRepetitiva.java
+++ EstructuraRepetitiva.java
@@ -4,64 +4,55 @@
  */
 package variablesitipusdades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mister Andriy
  */
-public class EstructuraRepetitiva {
+public class CalculadoraSimple {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //17/10/2022
-        // volem escriure per pantalla els 5 primers numeros 
         
-        System.out.println(1);
-        System.out.println("2");
-        System.out.println('3');
-        System.out.println("4");
-        System.out.println("5");
+        Scanner teclat = new Scanner(System.in);
         
-        // ara volem escriure els 1000 primers numeros
-        // farem servir una estructura reretitiva while
+        System.out.println("Primer número: ");
+        int num1 = teclat.nextInt();
         
-        //inicialitzem algunes variables
-        int num = 1;
+        System.out.println("Operació: ");
+        char op = teclat.next().charAt(0);
         
-        while (num <= 1000) {
-            // executa aquest bloc d'instruccions
-            System.out.println(num);
-            // ha d'haver algun canvi de variable que porti a la condicio a fals
-            // si no es quedaria en bucle infinit
-            num++; // num = num + 1
-        }
+        System.out.println("Segon número: ");
+        int num2 = teclat.nextInt();
         
-        // visualizar els numeros del 1000 al 0
-        // cal vigilar amb el valor d'inici, la condicio i l'increment
-        int num1 = 1000;
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacio = num1 * num2;
+        int divisio = num1 / num2;
+        int modul = num1 % num2;
         
-        while (num1 >= 0) {
-            // executa aquest bloc d'instruccions
-            System.out.println(num1);
-            // ha d'haver algun canvi de variable que porti a la condicio a fals
-            // si no es quedaria en bucle infinit
-            num1--; // num = num + 1
-        }
-        
-        //imprimir nomes els valors parells
-         int num2 = 0;
-        
-        while (num2 < 1000) {
-            // executa aquest bloc d'instruccions
-            if (num2 % 2 == 0) {
-                System.out.println(num2);
-            }
-            // ha d'haver algun canvi de variable que porti a la condicio a fals
-            // si no es quedaria en bucle infinit
-            num2++; // num = num + 1
-        }
+        switch (op){
+            case '+':
+                System.out.println("Resultat: " + suma);
+                break;
+            case '-':
+                System.out.println("Resultat: " + resta);
+                break;
+            case '*':
+                System.out.println("Resultat: " + multiplicacio);
+                break;
+            case '/':
+                System.out.println("Resultat: " + divisio);
+                break;    
+            case '%':
+                System.out.println("Resultat: " + modul);
+                break;
+      
+    }
     }
     
 }
